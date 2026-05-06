04cfc93fca647cf024d8c3ab96ee8e4dd34fb7d8
marcofilho <marco.filho@ess.eu>
2026-05-06 21:44:38 +0200

    Fix wrong indexation in packet_count counting

--- skadi_analysis/SkadiAnalyze.py
+++ skadi_analysis/SkadiAnalyze.py
@@ -27,7 +27,7 @@ def main():
             p = GenericPacket(packet)
             p.pretty_print(readout_number=args.readouts)
             packet_count += 1
-        print(f"Analyzed {packet_count + 1} packets")
+        print(f"Analyzed {packet_count} packets")
 
 if __name__ == "__main__":
     main()
