4300307880dd3a65075e28ebe3666562d0cdf9b1
marcofilho <marco.filho@ess.eu>
2026-05-09 15:32:33 +0200

    Add analyzer class to do data decoding.
    Chose to make "print_packets" make the decoding again instead of storing
    packets in memory because this will decode big files and printing
    packets is not the priority.

---
+++ skadi_analysis/Analyzer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+from scapy.all import PcapReader
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+
+from .GenericPacket import GenericPacket
+
+class Analyzer:
+
+	"""
+	Receives a list of pcap filenames, compile them into statistics.
+
+	files: list of file names
+	resolution for event per time histogram
+
+	Variables in data:
+		PacketTypes: number of packet for each type in total
+		files: list of files analyzed
+		ReadoutNUmber: list of number of readouts per packet
+		PacketTimestamps: list of packet arrival times
+	"""
+
+	def __init__(self, *files):
+
+		self.data = {"PacketTypes": {"Non-17": 0, "Short-UDP": 0, "MDNS": 0, "Unknown": 0, "Skadi-RMM": 0},
+			   		 "files": [], "ReadoutNumber": [], "PacketTimestamps": []}
+
+		for file in files:
+			self.data["files"].append(file)
+
+
+	def decode(self):
+		"""
+		Reads packet by packet, stored important info
+		"""
+		for file in self.data["files"]:
+			with PcapReader(file) as pcap:
+				for packet in pcap:
+					p = GenericPacket(packet)
+					self.data["PacketTypes"][p.data["packet_type"]]+=1
+					if p.data["packet_type"]!="Skadi-RMM":
+						continue
+
+					self.data["ReadoutNumber"].append(len(p.readouts))
+					self.data["PacketTimestamps"].append(p.data["pkt_arrival_time"])
+
+	def print_packets(self, start, number = None, readouts = 0):
+		"""
+		Prints n packets starting from packet number <start> (0 indexation)
+		"""
+		for file in self.data["files"]:
+			with PcapReader(file) as pcap:
+				packet_count = 0
+				for packet in pcap:
+					if number is not None and packet_count - start >= number:
+						break
+					if start is not None and packet_count < start:
+						packet_count += 1
+						continue
+					p = GenericPacket(packet)
+					p.pretty_print(readout_number=readouts)
+					packet_count += 1
+				print(f"Printed {packet_count} packets")
+
+	def plot_arrival_times(self, bin_number = 100):
+		fig, axs = plt.subplots(1, 2)
+		axs[0].hist(self.data["PacketTimestamps"], bins = bin_number)
+		axs[0].set_title("Packets per unix timestamp")
+
+		#Now for readouts
+		timestamps = np.array(self.data["PacketTimestamps"])
+		readouts = np.array(self.data["ReadoutNumber"])
+		bin_size = (timestamps.max() - timestamps.min())/bin_number
+		bins = np.linspace(timestamps.min(), timestamps.max(), bin_number + 1)
+		y, edges = np.histogram(self.data["PacketTimestamps"], bins=bins, weights=readouts)
+
+		axs[1].bar(edges[:-1], y, width=bin_size, align='edge')
+		axs[1].set_title("Readouts per unix timestamp")
+
+		plt.show()
--- skadi_analysis/SkadiAnalyze.py
+++ skadi_analysis/SkadiAnalyze.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
 
-from scapy.all import PcapReader, ls
+from scapy.all import PcapReader
 from argparse import ArgumentParser
 from .GenericPacket import GenericPacket
-
+from .Analyzer import Analyzer
 
 def main():
     parser = ArgumentParser(description="Analyze PCAP files")
     parser.add_argument("--file", "-f", required=True, help="Path to the PCAP file to analyze")
     parser.add_argument("--number", "-n", type=int, default=None, 
-                        help="Number of packets to read. (None for all)")
+                        help="Number of packets to print. (None for all)")
+    parser.add_argument("--print", "-p", help="Print packet info. Default = false.", 
+                        default=False, action = 'store_true')
     parser.add_argument("--start", "-s", type=int, default=0, 
                         help="Starting packet number (0 indexation).")
     parser.add_argument("--readouts", "-r", type=int, default=0, 
                         help="Number of readouts to print for each packet. (None for all)")
     args = parser.parse_args()
 
-    packet_count = 0
-    with PcapReader(args.file) as pcap:
-        for packet in pcap:
-            if args.number is not None and packet_count - args.start >= args.number:
-                break
-            if args.start is not None and packet_count < args.start:
-                packet_count += 1
-                continue
-            p = GenericPacket(packet)
-            p.pretty_print(readout_number=args.readouts)
-            packet_count += 1
-        print(f"Analyzed {packet_count} packets")
+    a = Analyzer(args.file)
+
+    if args.print:
+        a.print_packets(args.start, args.number, args.readouts)
 
 if __name__ == "__main__":
     main()
