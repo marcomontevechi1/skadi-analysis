71b9c928ed4d7e2348e4b48c49682e1f1e570096
marcofilho <marco.filho@ess.eu>
2026-05-12 22:56:16 +0200

    Abstract decoding from plot and printing
    This will make it much easier to plot number of events per channel,
    reusing the code we already have.
    
    Unfortunately, the linter and formatter decided to format the whole code
    for pre-commit, which makes the commit sorta unreadable. This is
    hopefully the first and last time this problem will happen here.
    
    If you need to compare this commit with previous commits, I suggest
    manually running them through whatever formatter you find...

--- skadi_analysis/Analyzer.py
+++ skadi_analysis/Analyzer.py
@@ -10,170 +10,192 @@ from .Readout import Readout
 
 MAX_ADC_HEIGHT = 65535
 
+
 class Analyzer:
+    """
+    Receives a list of pcap filenames, compile them into statistics.
 
-	"""
-	Receives a list of pcap filenames, compile them into statistics.
+    Due to the files being big, most of the functions decode in execution time and store only what they
+    must in memory. This is slow, but we are limited in RAM so...
 
-	Due to the files being big, most of the functions decode in execution time and store only what they
-	must in memory. This is slow, but we are limited in RAM so...
+    files: list of file names
+    resolution for event per time histogram
 
-	files: list of file names
-	resolution for event per time histogram
+    Variables in data:
+            PacketTypes: number of packet for each type in total
+            files: list of files analyzed
+            ReadoutNUmber: list of number of readouts per packet
+            PacketTimestamps: list of packet arrival times
+    self.boards: dict of board objects. Should be dynamically filled by self.decode()
+    """
 
-	Variables in data:
-		PacketTypes: number of packet for each type in total
-		files: list of files analyzed
-		ReadoutNUmber: list of number of readouts per packet
-		PacketTimestamps: list of packet arrival times
-	self.boards: dict of board objects. Should be dynamically filled by self.decode()
-	"""
+    def __init__(self, verbose=False, channel=None, bin_size=None, *files):
+        self.data = {
+            "PacketTypes": {
+                "Non-17": 0,
+                "Short-UDP": 0,
+                "MDNS": 0,
+                "Unknown": 0,
+                "Skadi-RMM": 0,
+            },
+            "files": [],
+            "ReadoutNumber": [],
+            "PacketTimestamps": [],
+        }  # Should be deprecated in favor of self.packets_stats
+        self.boards = dict()
+        self.packets_stats = {
+            "OM1": 0,
+            "OM2": 0,
+            "OM3": 0,
+            "Non-17": 0,
+            "Short-UDP": 0,
+            "MDNS": 0,
+            "Unknown": 0,
+            "Skadi-RMM": 0,
+        }
+        self.analyzer = {"BinSize": bin_size, "Files": list(files)}
+        for evt_type in Readout.EVENT_TYPES.values():
+            self.packets_stats[evt_type] = 0
+        for evt_type in Readout.EVENT_TYPES_OM0.values():
+            self.packets_stats[evt_type] = 0
 
-	def __init__(self, verbose = False, *files):
+        self.verbose = verbose
+        self.channel = channel
+        self.bin_size = bin_size
 
-		self.data = {"PacketTypes": {"Non-17": 0, "Short-UDP": 0, "MDNS": 0, "Unknown": 0, "Skadi-RMM": 0},
-			   		 "files": [], "ReadoutNumber": [], "PacketTimestamps": []}
-		self.boards = dict()
-		self.verbose = verbose
+        for file in files:
+            self.data["files"].append(file)
 
-		for file in files:
-			self.data["files"].append(file)
+    def decode_timestamps(self):  # Should be deprecated soon
+        """
+        Reads packet by packet, stored important info
+        """
+        packet_count = 0
+        for p in self.iterate_packets():
+            self.data["PacketTypes"][p.data["packet_type"]] += 1
+            if p.data["packet_type"] != "Skadi-RMM":
+                continue
 
-	def decode(self):
-		"""
-		Reads packet by packet, stored important info
-		"""
-		for p in self.iterate_packets():
-			self.data["PacketTypes"][p.data["packet_type"]]+=1
-			if p.data["packet_type"]!="Skadi-RMM":
-				continue
+            self.data["ReadoutNumber"].append(len(p.readouts))
+            self.data["PacketTimestamps"].append(p.data["pkt_arrival_time"])
 
-			self.data["ReadoutNumber"].append(len(p.readouts))
-			self.data["PacketTimestamps"].append(p.data["pkt_arrival_time"])
+            packet_count += 1
+            if self.verbose:
+                print(f"\rFinished decoding packet {packet_count}", end="", flush=True)
 
-			packet_count += 1
-			if self.verbose:
-				print(f"\rFinished decoding packet {packet_count}", end='', flush=True)
+    def print_packets(self, start, number=None, readouts=0):
+        """
+        Prints n packets starting from packet number <start> (0 indexation)
+        """
+        packet_count = 0
+        for packet in self.iterate_packets():
+            if number is not None and packet_count - start >= number:
+                break
+            if start is not None and packet_count < start:
+                packet_count += 1
+                continue
+            packet.pretty_print(readout_number=readouts)
+            packet_count += 1
+        print(f"Printed {packet_count} packets")
 
-	def print_packets(self, start, number = None, readouts = 0):
-		"""
-		Prints n packets starting from packet number <start> (0 indexation)
-		"""
-		packet_count = 0
-		for packet in self.iterate_packets():
-			if number is not None and packet_count - start >= number:
-				break
-			if start is not None and packet_count < start:
-				packet_count += 1
-				continue
-			packet.pretty_print(readout_number=readouts)
-			packet_count += 1
-		print(f"Printed {packet_count} packets")
+    def plot_arrival_times(self, bin_number=100):
+        fig, axs = plt.subplots(1, 2)
+        axs[0].hist(self.data["PacketTimestamps"], bins=bin_number)
+        axs[0].set_title("Packets per unix timestamp")
 
-	def plot_arrival_times(self, bin_number = 100):
-		fig, axs = plt.subplots(1, 2)
-		axs[0].hist(self.data["PacketTimestamps"], bins = bin_number)
-		axs[0].set_title("Packets per unix timestamp")
+        # Now for readouts
+        timestamps = np.array(self.data["PacketTimestamps"])
+        readouts = np.array(self.data["ReadoutNumber"])
+        bin_size = (timestamps.max() - timestamps.min()) / bin_number
+        bins = np.linspace(timestamps.min(), timestamps.max(), bin_number + 1)
+        y, edges = np.histogram(
+            self.data["PacketTimestamps"], bins=bins, weights=readouts
+        )
 
-		#Now for readouts
-		timestamps = np.array(self.data["PacketTimestamps"])
-		readouts = np.array(self.data["ReadoutNumber"])
-		bin_size = (timestamps.max() - timestamps.min())/bin_number
-		bins = np.linspace(timestamps.min(), timestamps.max(), bin_number + 1)
-		y, edges = np.histogram(self.data["PacketTimestamps"], bins=bins, weights=readouts)
+        axs[1].bar(edges[:-1], y, width=bin_size, align="edge")
+        axs[1].set_title("Readouts per unix timestamp")
 
-		axs[1].bar(edges[:-1], y, width=bin_size, align='edge')
-		axs[1].set_title("Readouts per unix timestamp")
+        plt.show()
 
-		plt.show()
+    def decode(self):
+        """
+        Decodes packets, stores ADC values in binned containers.
+        Bin size is size of bins so that bin_number can be adjusted automatically accordingly.
 
-	def plot_board_adc(self, bin_size, channel = None, dumpfile = None):
-		"""
-		Decodes packets, plots a histogram of ADCs with bins size of bin_size.
-		Bin size is size of bins so that bin_number can be adjusted automatically accordingly.
+        There are easier ways of doing this, but this is done in order to allow for
+        opening big files without using all system's RAM.
+        """
 
-		There are easier ways of doing this, but this is done in order to allow for
-		opening big files without using all system's RAM.
-		"""
+        pkt_count = 0
 
-		pkt_count = 0
-		boards = dict() #board[boardidx] = chan. chan[idx] = binned.
-		packet_info = {"OM1": 0, "OM2": 0, "OM3": 0, "Non-17": 0,
-				 	   "Short-UDP": 0, "MDNS": 0, "Unknown": 0, "Skadi-RMM": 0}
+        for p in self.iterate_packets():
+            self.packets_stats[p.data["packet_type"]] += 1
+            if p.data["packet_type"] != "Skadi-RMM":
+                continue
 
-		for evt_type in Readout.EVENT_TYPES.values():
-			packet_info[evt_type] = 0
-		for evt_type in Readout.EVENT_TYPES_OM0.values():
-			packet_info[evt_type] = 0
+            for readout in p.readouts:
+                self.packets_stats[readout.data["EvtType"]] += 1
+                self.packets_stats[f"OM{readout.data['OM']}"] += 1
 
-		for p in self.iterate_packets():
-			packet_info[p.data["packet_type"]]+=1
-			if p.data["packet_type"]!="Skadi-RMM":
-				continue
+                octet = readout.data["IPLastOctet"]
+                ch = readout.data["Channel"]
+                if octet not in self.boards.keys():
+                    self.boards[octet] = np.zeros(
+                        (256, MAX_ADC_HEIGHT // self.bin_size + 1), dtype=int
+                    )
+                self.boards[octet][ch][readout.data["ADC"] // self.bin_size] += 1
 
-			for readout in p.readouts:
-				packet_info[readout.data["EvtType"]]+=1
-				packet_info[f"OM{readout.data['OM']}"]+=1
+            pkt_count += 1
+            if self.verbose:
+                print(f"\rFinished decoding packet {pkt_count}", end="", flush=True)
 
-				octet = readout.data["IPLastOctet"]
-				ch = readout.data["Channel"]
-				if octet not in boards.keys():
-					boards[octet] = np.zeros((256, MAX_ADC_HEIGHT//bin_size + 1), dtype=int)
-				boards[octet][ch][readout.data["ADC"]//bin_size]+=1
-		
-			pkt_count += 1
-			if self.verbose:
-				print(f"\rFinished decoding packet {pkt_count}", end='', flush=True)
-				
-		if self.verbose:
-			print("")
+        if self.verbose:
+            print("")
 
-		if channel is None:
-			for octet in boards.keys():
-				boards[octet] = np.sum(boards[octet], axis=0)
-		else:
-			for octet in boards.keys():
-				boards[octet] = boards[octet][channel]
+    def print_packets_stats(self):
+        print("Packets stats:")
+        print(
+            yaml.dump(self.packets_stats, allow_unicode=True, default_flow_style=False)
+        )
 
-		print("Packets info:")
-		print(yaml.dump(packet_info, allow_unicode=True, default_flow_style=False))
-		self.plot_ADC_boards(boards, bin_size)
-		boards["Bin size"] = bin_size
-		boards["Channel"] = channel
-		boards["Files"] = self.data["files"]
-		self.dump_ADC_file(dict(boards, **packet_info), dumpfile)
+    def plot_ADC_boards(self):
+        if self.channel is None:
+            for octet in self.boards.keys():
+                self.boards[octet] = np.sum(self.boards[octet], axis=0)
+        else:
+            for octet in self.boards.keys():
+                self.boards[octet] = self.boards[octet][self.channel]
 
-	def plot_ADC_boards(self, boards, bin_size):
-		fig, ax = plt.subplots(len(boards.keys())//4 + 1, 4)
-		for i, octet in enumerate(sorted(boards)):
-			row = i//4
-			col = i%4
-			largest_nonzero_index = np.max(np.nonzero(boards[octet]))
-			binned = boards[octet][:largest_nonzero_index+1]
-			edges = np.arange(len(binned) + 1) * bin_size
-			ax[row][col].stairs(binned, edges)
-			ax[row][col].set_title(f"Board {octet}")
-		plt.suptitle("ADC PulseHeight distribution")
-		plt.show()
+        fig, ax = plt.subplots(len(self.boards.keys()) // 4 + 1, 4)
+        for i, octet in enumerate(sorted(self.boards)):
+            row = i // 4
+            col = i % 4
+            largest_nonzero_index = np.max(np.nonzero(self.boards[octet]))
+            binned = self.boards[octet][: largest_nonzero_index + 1]
+            edges = np.arange(len(binned) + 1) * self.bin_size
+            ax[row][col].stairs(binned, edges)
+            ax[row][col].set_title(f"Board {octet}")
+        plt.suptitle("ADC PulseHeight distribution")
+        plt.show()
 
-	def dump_ADC_file(self, boards, filename):
-		if filename is not None:
-			if self.verbose:
-				print(f"Dumping data to {filename}...")
-			for key in boards.keys():
-				if isinstance(boards[key],np.ndarray):
+    def dump_ADC_file(self, filename):
+        data_to_dump = self.boards | self.packets_stats | self.analyzer
+        if filename is not None:
+            if self.verbose:
+                print(f"Dumping data to {filename}...")
+            for key in data_to_dump.keys():
+                if isinstance(data_to_dump[key], np.ndarray):
+                    data_to_dump[key] = data_to_dump[key].tolist()
+            with open(filename, "w") as file:
+                file.write(yaml.dump(data_to_dump))
+            if self.verbose:
+                print("Dumped data successfully.")
 
-					boards[key] = boards[key].tolist()
-			with open(filename, 'w') as file:
-				file.write(yaml.dump(boards))
-			if self.verbose:
-				print("Dumped data successfully.")
-
-	def iterate_packets(self):
-		"""
-		Generator that iterates through all packets in all files, yielding a GenericPacket object.
-		"""
-		for file in self.data["files"]:
-			with PcapReader(file) as pcap:
-				for packet in pcap:
-					yield GenericPacket(packet)
+    def iterate_packets(self):
+        """
+        Generator that iterates through all packets in all files, yielding a GenericPacket object.
+        """
+        for file in self.data["files"]:
+            with PcapReader(file) as pcap:
+                for packet in pcap:
+                    yield GenericPacket(packet)
--- skadi_analysis/SkadiAnalyze.py
+++ skadi_analysis/SkadiAnalyze.py
@@ -1,42 +1,104 @@
 #!/usr/bin/env python3
 
-from scapy.all import PcapReader
 from argparse import ArgumentParser
-from .GenericPacket import GenericPacket
 from .Analyzer import Analyzer
+
 
 def main():
     parser = ArgumentParser(description="Analyze PCAP files")
-    parser.add_argument("--file", "-f", required=True, help="Path to the PCAP file to analyze")
-    parser.add_argument("--number", "-n", type=int, default=None, 
-                        help="Number of packets to print. (None for all)")
-    parser.add_argument("--print", "-p", help="Print packet info. Default = false.", 
-                        default=False, action = 'store_true')
-    parser.add_argument("--start", "-s", type=int, default=0, 
-                        help="Starting packet number (0 indexation).")
-    parser.add_argument("--readouts", "-r", type=int, default=0, 
-                        help="Number of readouts to print for each packet. (None for all)")
-    parser.add_argument("--plot-packets", help="Plots number of packets and readout in time. Should have a bin number.",
-                        default = False, action = 'store_true')
-    parser.add_argument("--plot-adc-board", help="Plots pulseheight distribution for a given board. See also bin-size.", 
-                        default=False, action = 'store_true')
-    parser.add_argument("--bin-number", "-b", help="Bin number for timestamp plotting.", type=int)
-    parser.add_argument("--bin-size", help="Size of bin for ADC plotting. Bin number will be plotted automatically.", type=int, default=1)
-    parser.add_argument("--channel", "-c", help="Channel for plotting pulseHeight distribution.", type=int, default=None)
-    parser.add_argument("--verbose", "-v", help="Talk about decoding progress", default=False, action = 'store_true')
-    parser.add_argument("--dump", "-d", help="Filename to dump info in", type=str, default=None)
+    parser.add_argument(
+        "--file", "-f", required=True, help="Path to the PCAP file to analyze"
+    )
+    parser.add_argument(
+        "--number",
+        "-n",
+        type=int,
+        default=None,
+        help="Number of packets to print. (None for all)",
+    )
+    parser.add_argument(
+        "--print-packets",
+        "-p",
+        help="Print packet info for each packet. Default = false.",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--print-packets-stats",
+        "-ps",
+        help="Print packet statistics. Default = false.",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--start",
+        "-s",
+        type=int,
+        default=0,
+        help="Starting packet number (0 indexation).",
+    )
+    parser.add_argument(
+        "--readouts",
+        "-r",
+        type=int,
+        default=0,
+        help="Number of readouts to print for each packet. (None for all)",
+    )
+    parser.add_argument(
+        "--plot-packets",
+        help="Plots number of packets and readout in time. Should have a bin number.",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--plot-adc-board",
+        help="Plots pulseheight distribution for a given board. See also bin-size.",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--bin-number", "-b", help="Bin number for timestamp plotting.", type=int
+    )
+    parser.add_argument(
+        "--bin-size",
+        help="Size of bin for ADC plotting. Bin number will be plotted automatically.",
+        type=int,
+        default=1,
+    )
+    parser.add_argument(
+        "--channel",
+        "-c",
+        help="Channel for plotting pulseHeight distribution.",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Talk about decoding progress",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--dump", "-d", help="Filename to dump info in", type=str, default=None
+    )
     args = parser.parse_args()
 
-    a = Analyzer(args.verbose, args.file)
+    a = Analyzer(args.verbose, args.channel, args.bin_size, args.file)
+    a.decode()
 
-    if args.print:
+    if args.print_packets:
         a.print_packets(args.start, args.number, args.readouts)
     if args.plot_packets:
-        a.decode()
+        a.decode_timestamps()
         a.plot_arrival_times(bin_number=args.bin_number)
     if args.plot_adc_board:
-        a.plot_board_adc(args.bin_size, args.channel, args.dump)
+        a.plot_ADC_boards()
+    if args.print_packets_stats:
+        a.print_packets_stats()
+    if args.dump is not None:
+        a.dump_ADC_file(args.dump)
+
 
 if __name__ == "__main__":
     main()
-
